d4f2e620256bfd120783f5d167c35b4efcbf35d2
Sooshimi <59628017+Sooshimi@users.noreply.github.com>
2023-09-29 17:22:55 +0100

    WIP for meteor and laser interaction

--- laser.gd
+++ laser.gd
@@ -3,6 +3,8 @@ extends Area2D
 @export var speed = 1000
 var vector = Vector2.ZERO
 
+signal laser_hit
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -13,4 +15,10 @@ func _process(delta):
 	global_position += vector.rotated(rotation) * speed * delta
 
 func _on_visible_on_screen_notifier_2d_screen_exited():
+	queue_free()
+
+func _on_body_entered(body):
+	print("hitbody")
+	laser_hit.emit()
+	$CollisionShape2D.set_deferred("disabled", true)
 	queue_free()
