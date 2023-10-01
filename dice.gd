27d9e1ad862e5140fec07df2e14c1c43f936b266
Sooshimi <59628017+Sooshimi@users.noreply.github.com>
2023-10-01 18:31:40 +0100

    Add die roll ability for side 6

--- dice.gd
+++ dice.gd
@@ -7,6 +7,8 @@ var side_four = load("res://assets/dice/dice_4.png")
 var side_five = load("res://assets/dice/dice_5.png")
 var side_six = load("res://assets/dice/dice_6.png")
 
+var rolled_index
+var rolled_side
 var screen_size
 var dice_sides = [side_one, side_two, side_three, side_four, side_five, side_six]
 
@@ -18,8 +20,13 @@ func _integrate_forces(state):
 
 func roll():
 	for i in 5:
-		$Sprite2D.texture = dice_sides[randi() % dice_sides.size()]
+		rolled_index = randi() % dice_sides.size()
+		rolled_side = rolled_index + 1
+		$Sprite2D.texture = dice_sides[rolled_index]
 		await $RollAnimationTimer.timeout
+	
+	if rolled_side == 6:
+		get_tree().call_group("meteors", "queue_free")
 
 func _on_body_entered(body):
 	roll()
